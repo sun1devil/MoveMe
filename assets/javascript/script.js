97c9437dd62bedd9a0a9d980d95ba589d1e604df
sun1devil <codejmc11@gmail.com>
2018-05-10 14:56:58 -0700

    update firebase logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,9 +8,32 @@
 //                   John
 // ========================================================
 
- var john = awesome;
+var database = firebase.database();
 
- ggggggg
+//on click search capture zipcode count each//
+$("#user-zip-submit").on("click", function(event) 
+{
+    event.preventDefault();
+    
+    var userZip = $("#user-zip").val().trim();
+    var countZip = 0;
+    $("#user-zip").val("");
+    database.ref().push(userZip);
+    console.log(userZip);
+    countZip++;
+    console.log(countZip);
+    
+    
+})
+    database.ref().on("child_added", function(childSnapshot, prevChildKey)
+    {
+        var userZip = childSnapshot.val().zip;
+        // var 
+        console.log(childSnapshot.val());
+    })
+
+
+ 
 
 // ========================================================
 //                   Mindy
