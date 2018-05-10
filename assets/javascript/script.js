cc71c9a7975f2a131294b7a87583392f8deaac2b
Robert Shaw <robertshaw87@gmail.com>
2018-05-10 12:16:27 -0800

    add placeholder data for meetupList

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,11 +26,28 @@ var userZip;
 // ========================================================
 //                   Robert
 // ========================================================
-var meetupList = [{
+meetupList = [{
     eventName: "event placeholder",
+    descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus dolor vitae lacus suscipit mattis. Donec sed sem tempus, viverra neque non, consectetur sem. Donec lacinia mauris eget maximus blandit.",
     date: "08/02/2018",
     attending: "200",
     image: "#",
     lat: "109.7",
+    long: "-117.1"
+}, {
+    eventName: "event placeholder 2",
+    descrip: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus id gravida orci. Mauris at tincidunt mauris. Nam ultricies libero velit, eu vulputate est sodales ut. Fusce eu magna eget purus malesuada ullamcorper.",
+    date: "07/22/2018",
+    attending: "324",
+    image: "#",
+    lat: "119.2",
+    long: "-117.4"
+}, {
+    eventName: "event placeholder 3",
+    descrip: "Quisque luctus eros sit amet mollis porta. Phasellus ut massa sed diam faucibus ultrices quis non est. Duis viverra sagittis ligula, at malesuada arcu venenatis vel. Sed pulvinar interdum nibh, a condimentum augue pretium nec. ",
+    date: "07/13/2018",
+    attending: "132",
+    image: "#",
+    lat: "102.1",
     long: "-117.1"
 }]
