faf93c4dd883f5817767543d1176e5a4e6f990a1
Robert Shaw <robertshaw87@gmail.com>
2018-05-10 13:16:49 -0800

    add header and spacing to dynamically generated event cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,7 +31,7 @@ meetupList = [{
     descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus dolor vitae lacus suscipit mattis. Donec sed sem tempus, viverra neque non, consectetur sem. Donec lacinia mauris eget maximus blandit.",
     date: "08/02/2018",
     attending: "200",
-    image: "#",
+    image: "http://via.placeholder.com/200x200",
     lat: "109.7",
     long: "-117.1"
 }, {
@@ -39,7 +39,7 @@ meetupList = [{
     descrip: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus id gravida orci. Mauris at tincidunt mauris. Nam ultricies libero velit, eu vulputate est sodales ut. Fusce eu magna eget purus malesuada ullamcorper.",
     date: "07/22/2018",
     attending: "324",
-    image: "#",
+    image: "http://via.placeholder.com/200x200",
     lat: "119.2",
     long: "-117.4"
 }, {
@@ -47,7 +47,37 @@ meetupList = [{
     descrip: "Quisque luctus eros sit amet mollis porta. Phasellus ut massa sed diam faucibus ultrices quis non est. Duis viverra sagittis ligula, at malesuada arcu venenatis vel. Sed pulvinar interdum nibh, a condimentum augue pretium nec. ",
     date: "07/13/2018",
     attending: "132",
-    image: "#",
+    image: "http://via.placeholder.com/200x200",
     lat: "102.1",
-    long: "-117.1"
+    long: "-104.7"
 }]
+function displayMeetups () {
+    $("#event-content").empty();
+    for (var i=0; i<meetupList.length; i++){
+        var currObj = meetupList[i];
+        var eventWrapper = $("<div>");
+        eventWrapper.addClass("mt-3 mr-3");
+        
+        var eventCard = $("<div>");
+        eventCard.addClass("card col m-2 position-relative");
+
+        var eventCardHeader = $("<div>");
+        eventCardHeader.addClass("card-header row bg-dark text-light p-2");
+        var eventCardHeaderName = $("<h5>");
+        eventCardHeaderName.addClass("col-8");
+        eventCardHeaderName.text(currObj.eventName);
+        var eventCardHeaderDate = $("<h6>");
+        eventCardHeaderDate.addClass("col-4 text-right");
+        eventCardHeaderDate.text(currObj.date);
+        eventCardHeader.append(eventCardHeaderName);
+        eventCardHeader.append(eventCardHeaderDate)
+        eventCard.append(eventCardHeader);
+
+        eventCardBody = $("<div>");
+        eventCardBody.addClass("card-body row")
+
+        eventWrapper.append(eventCard);
+        $("#event-content").append(eventWrapper)
+    }
+}
+displayMeetups();
